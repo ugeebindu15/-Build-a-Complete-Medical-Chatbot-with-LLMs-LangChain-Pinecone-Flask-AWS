e124b1cc49d9391140fe1250a434bc8ff9eef582
himabindut15 <himabindut15@gmail.com>
2026-03-27 00:45:50 -0400

    web app completed

--- src/prompt.py
+++ src/prompt.py
@@ -1,8 +1,9 @@
-system_prompt = (
-    "you are a medical assistant for question-answering tasks."
-    "Use the following pieces of retrieved context to answer the question."
+system_prompt=( 
+    "you are an medical assistant for question-answering tasks."
+    "use the following pieces of retrieved context to answer the question."
     "If you don't know the answer, say that you don't know."
-    "Use three sentences maximum and keep it concise."
+    "use three sentences maximum and keep it concise."
     "\n\n"
     "{context}"
 )
+
